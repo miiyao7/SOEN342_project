98747dd8b21085557131a742f01649d6228c7e6c
Draya <56936757+Andrea641@users.noreply.github.com>
2025-10-17 18:25:15 -0400

    git ignore

--- Backend/target/debug/deps/unicode_ident-189ca15178b593cc.d
+++ Backend/target/debug/deps/unicode_ident-189ca15178b593cc.d
@@ -1,8 +1,8 @@
-C:\Users\mai\Documents\GitHub\SOEN342_project\backend\target\debug\deps\unicode_ident-189ca15178b593cc.d: C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
+D:\Andrea\Documents\GitHub\SOEN342_project\Backend\target\debug\deps\unicode_ident-189ca15178b593cc.d: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
 
-C:\Users\mai\Documents\GitHub\SOEN342_project\backend\target\debug\deps\libunicode_ident-189ca15178b593cc.rlib: C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
+D:\Andrea\Documents\GitHub\SOEN342_project\Backend\target\debug\deps\libunicode_ident-189ca15178b593cc.rlib: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
 
-C:\Users\mai\Documents\GitHub\SOEN342_project\backend\target\debug\deps\libunicode_ident-189ca15178b593cc.rmeta: C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
+D:\Andrea\Documents\GitHub\SOEN342_project\Backend\target\debug\deps\libunicode_ident-189ca15178b593cc.rmeta: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
 
-C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs:
-C:\Users\mai\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs:
+C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs:
+C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs:
